2dfe02f49d9029476ff89330704fb93adc03d5c5
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-08-20 15:43:19 +0200

    CS

--- locales/en.php
+++ locales/en.php
@@ -4,8 +4,8 @@ return array(
     'error' => array(
         'tableFound'  => 'Table "%table%" must NOT exist in the database.',
         'tableNotFound' => 'Table "%table%" not found in the database.',
-        'columnFound'  => 'Column "%column%" must NOT exist in the table "%table%".',
-        'columnNotFound'  => 'Column "%column%" not found in the table "%table%".',
+        'columnFound' => 'Column "%column%" must NOT exist in the table "%table%".',
+        'columnNotFound' => 'Column "%column%" not found in the table "%table%".',
     ),
     'confirm' => array(
         'configfree' => 'There is no configuration necessary for this migration step.',
