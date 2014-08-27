a71ee5799f9da368d5074f418dab62213a7ec53e
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-08-27 17:06:08 +0200

    Error reporting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,10 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 $autoload = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($autoload)) {
     throw new RuntimeException('Install dependencies to run test suite.');
