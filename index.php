cdbe2a5f29a17de949b89a4d96d9726989aabfe7
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-10-19 20:41:49 +0200

    Hide PHP notice if debug mode is disabled

--- index.php
+++ index.php
@@ -12,10 +12,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
-
-//ini_set('error_reporting', E_ALL & ~E_NOTICE);
-$app['debug'] = true; // TODO: disable debug mode for stable version
 $app['kernel.root_dir'] = __DIR__;
+
+$app['debug'] = true; // TODO: disable debug mode for stable version
+
+if (!$app['debug']) {
+    ini_set('error_reporting', E_ALL & ~E_NOTICE);
+}
 
 // Support PHAR
 if (\Phar::running()) {
