c6afec68de5c4708ac8aa7898b6ef37cf9282c1a
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-10-19 16:20:26 +0200

    Zero was no accepted as valid value

--- services/ShopConfigMigrationService.php
+++ services/ShopConfigMigrationService.php
@@ -366,8 +366,8 @@ class ShopConfigMigrationService extends AbstractMigrationService
             $options[] = array(
                 'id'              => $row['id'],
                 'name'            => $row['name'],
-                'list_gallery'    => (string) ($config[$row['id']]['list_gallery'] ?: ''),
-                'reader_gallery'  => (string) ($config[$row['id']]['reader_gallery'] ?: ''),
+                'list_gallery'    => (isset($config[$row['id']]['list_gallery']) ? $config[$row['id']]['list_gallery'] : ''),
+                'reader_gallery'  => (isset($config[$row['id']]['reader_gallery']) ? $config[$row['id']]['reader_gallery'] : ''),
             );
         }
 
